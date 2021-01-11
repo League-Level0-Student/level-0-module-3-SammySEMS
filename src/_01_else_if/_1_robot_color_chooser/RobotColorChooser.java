e11726e307d6c612d1aa477e947c216a24ff4fab
root <root@9b00899db0f7>
2021-01-11 21:25:39 +0000

    High and low game

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -15,18 +15,46 @@ public class RobotColorChooser {
 	    //2. Make the robot draw a shape (this will take more than one line of code)
 	
 		//3. Set the pen width to 10
-		
+		rob.penDown();
+		rob.setPenWidth(10);
 		//4. Ask the user what color pen they would like the robot to draw with
-		
+		String answer =JOptionPane.showInputDialog("What color pen would you like the robot to draw with?");
 		
 		//5. Use an if/else statement to set the pen color that the user requested
-		
+		if (answer.equals("red"))
+		{
+			rob.setPenColor(Color.red);
+		}
+		else if (answer.equals("yellow"))
+		{
+			rob.setPenColor(Color.yellow);
+		}
+		else if (answer.equals("orange"))
+		{
+			rob.setPenColor(Color.orange);
+		}
+		else if (answer.equals("green"))
+		{
+			rob.setPenColor(Color.green);
+		}
+		else if (answer.equals("blue"))
+		{
+			rob.setPenColor(Color.blue);
+		}
+		else if (answer.equals("pink"))
+		{
+			rob.setPenColor(Color.pink);
+		}
+		else {
+			rob.setRandomPenColor();
+		}
         //6. If the user doesn't enter anything, choose a random color
 		
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		for (int i = 0; i < 4; i++) {
-			rob.move(20);
+			rob.setSpeed(20);
+			rob.move(200);
 			rob.turn(90);
 		}
 
--- src/_01_else_if/_3_high_low/HighLowGame.java
+++ src/_01_else_if/_3_high_low/HighLowGame.java
@@ -3,6 +3,8 @@ package _01_else_if._3_high_low;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
@@ -12,14 +14,18 @@ public class HighLowGame {
 		// 2. Print out the random variable above
 		
 		// 11. Repeat steps 1 to 10 ten times
-		
+		for (int i = 0; i < 10; i++){
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
+String answer= JOptionPane.showInputDialog("Guess a number!");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int Integer.parseInt(answer));
 			// 5. if the guess is correct
 				// 6. Win
+			if(answer=random.number) {
+				JOptionPane.showMessageDialog(null, "Win!");
+			}
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
+		
 			// 7. if the guess is high
 				// 8. Tell them it's too high
 			// 9. if the guess is low
